16b3ccbe53eac0df6f9cd16f5560873b9be30e90
Fiona <45771219+FionaXYZ@users.noreply.github.com>
2020-05-29 18:25:50 +0100

    my understanding of FPGA to CPU

---
+++ number_increment^2.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "../address_map_arm.h"
+#include <time.h>
+
+/* Prototypes for functions used to access physical memory addresses */
+int open_physical (int);
+void * map_physical (int, unsigned int, unsigned int);
+void close_physical (int);
+int unmap_physical (void *, unsigned int);
+void delay(int number_of_seconds);
+
+/* This program increments the contents of the red LED parallel port */
+int main(void)
+{
+   volatile int * LEDR_ptr;   // virtual address pointer to red LEDs
+   int fd = -1;               // used to open /dev/mem for access to physical addresses
+   void *LW_virtual;          // used to map physical addresses for the light-weight bridge
+   // Create virtual memory access to the FPGA light-weight bridge
+   if ((fd = open_physical (fd)) == -1)
+      return (-1);
+   if ((LW_virtual = map_physical (fd, LW_BRIDGE_BASE, LW_BRIDGE_SPAN)) == NULL)
+      return (-1);
+
+   // Set virtual address pointer to I/O port
+   LEDR_ptr = (unsigned int *) (LW_virtual + LEDR_BASE);
+    *LEDR_ptr = 0;
+    if(*LEDR_ptr==0){
+    *LEDR_ptr = 1;
+    }
+while(*LEDR_ptr<=512){
+   *LEDR_ptr = *LEDR_ptr*2;
+       delay(1000);
+   if (*LEDR_ptr>=512){
+       *LEDR_ptr=1;
+       delay(1000);
+   }
+   }
+   unmap_physical (LW_virtual, LW_BRIDGE_SPAN);   // release the physical-memory mapping
+   close_physical (fd);   // close /dev/mem
+   return 0;
+}
+
+// Open /dev/mem, if not already done, to give access to physical addresses
+int open_physical (int fd)
+{
+   if (fd == -1)
+      if ((fd = open( "/dev/mem", (O_RDWR | O_SYNC))) == -1)
+      {
+         printf ("ERROR: could not open \"/dev/mem\"...\n");
+         return (-1);
+      }
+   return fd;
+}
+
+// Close /dev/mem to give access to physical addresses
+void close_physical (int fd)
+{
+   close (fd);
+}
+
+/*
+ * Establish a virtual address mapping for the physical addresses starting at base, and
+ * extending by span bytes.
+ */
+void* map_physical(int fd, unsigned int base, unsigned int span)
+{
+   void *virtual_base;
+
+   // Get a mapping from physical addresses to virtual addresses
+   virtual_base = mmap (NULL, span, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, base);
+   if (virtual_base == MAP_FAILED)
+   {
+      printf ("ERROR: mmap() failed...\n");
+      close (fd);
+      return (NULL);
+   }
+   return virtual_base;
+}
+
+/*
+ * Close the previously-opened virtual address mapping
+ */
+int unmap_physical(void * virtual_base, unsigned int span)
+{
+   if (munmap (virtual_base, span) != 0)
+   {
+      printf ("ERROR: munmap() failed...\n");
+      return (-1);
+   }
+   return 0;
+}
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+} 
